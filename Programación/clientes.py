4dd217d3dc1c5f6b679565e5a8ba2eb28aa7333d
Brenda Amaya <brendaamaya9@gmail.com>
2024-06-12 00:10:53 -0300

    modificando index y clientes con lo que va a ser una consulta sql para todos los archivos

--- Programación/clientes.py
+++ Programación/clientes.py
@@ -1,34 +1,33 @@
-def agregar_cliente():
-    print("Función agregar_cliente llamada.")
+import baseDeDatos
 
 def mostrar_clientes():
-    print("Función mostrar_clientes llamada.")
+    query = "SELECT * FROM clientes"
+    baseDeDatos.cursor.execute(query)
+    lista_clientes = baseDeDatos.cursor.fetchall()
+    
+    for cliente in lista_clientes:
+        print(cliente)
+
 
 def modificar_cliente():
     print("Función modificar_cliente llamada.")
 
-def eliminar_cliente():
-    print("Función eliminar_cliente llamada.")
+def a_definir():
+    print("Función llamada.")
 
 def menu_clientes():
     while True:
         print("\nMenú Clientes:")
-        print("1. Agregar Cliente")
-        print("2. Mostrar Clientes")
-        print("3. Modificar Cliente")
-        print("4. Eliminar Cliente")
-        print("5. Volver al Menú Principal")
+        print("1. Mostrar Clientes.")
+        print("2. A definir.")
+        print("3. Volver al Menú Principal")
         opcion = input("Seleccione una opción: ")
         
         if opcion == "1":
-            agregar_cliente()
-        elif opcion == "2":
             mostrar_clientes()
+        elif opcion == "2":
+            a_definir()
         elif opcion == "3":
-            modificar_cliente()
-        elif opcion == "4":
-            eliminar_cliente()
-        elif opcion == "5":
             break
         else:
             print("Opción no válida. Intente de nuevo.")
